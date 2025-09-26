06a39ec07dbd8f25947eac76f653a62910e475d8
Gabriel Feippe <gabriel@feippe.com>
2025-09-25 21:31:21 -0300

    Cleanning to start again

--- src/lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export const prisma =
-  globalThis.prisma ??
-  new PrismaClient({
-    log: ["error", "warn"],
-  });
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma as any;
